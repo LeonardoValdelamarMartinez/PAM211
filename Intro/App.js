9ce33b4be89d3f68eaff1729060d5410f62b4588
LeonardoValdelamarMartinez <124048844@upq.edu.mx>
2025-10-07 17:03:55 -0600

    Practica 5
    En esta practica aprendimos la importación y el uso de un botón

--- Intro/App.js
+++ Intro/App.js
@@ -1,16 +1,27 @@
+//1. Zona de los imports (donde vamos a agregar lo que necesitemos) 
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import Reactimport , { useState } from 'react';
 
-export default function App() {
+//2. Zona de componentes
+export default function App() 
+{
+  const [contador, setContador] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Hola Mundo React Native!</Text>
+      <Text>Contador: {contador}</Text>
+      <Button title = 'Agregar' onPress={()=>setContador(contador+1)} />
+      <StatusBar style="auto" />
+      <Button title = 'Quitar' onPress={()=>setContador(contador-1)} />
+      <StatusBar style="auto" />
+      <Button title = 'Reiniciar' onPress={()=>setContador(0)} />
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
+//3. Zona de los estilos
+const styles = StyleSheet.create(
+{
   container: {
     flex: 1,
     backgroundColor: '#fff',
